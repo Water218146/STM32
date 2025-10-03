20e11e11d4737aedde62b514b8b8cb96edb337bd
water2181 <2181466558@qq.com>
2025-10-03 13:44:15 +0800

    Clean up root directory: move all project files to stm32_Button_and_Led-main subdirectory

--- APP/key_app.h
+++
@@ -1,9 +0,0 @@
-#ifndef __KEY_APP_H_
-#define __KEY_APP_H_
-
-#include "mydefine.h"
-
-void key_task(void);
-void my_ebtn_init(void);
-
-#endif
--- APP/led_app.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LED_APP_H_
-#define __LED_APP_H_
-
-#include "mydefine.h"
-
-void led_task(void);
-
-#endif
